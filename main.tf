2f3f7786abdc2e2eb68f5bfb9006c0745d35ff69
Brian Gomes <bgomes@skillstorm.com>
2023-05-31 10:52:09 -0500

    added routes and route tables

--- main.tf
+++ main.tf
@@ -35,3 +35,21 @@ resource "aws_subnet" "public2" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+    tags = {
+    Name = "bjgomes-publicroutetable"
+    env  = "Dev"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = [aws_subnet.public1.id, aws_subnet.public2.id]
+}
