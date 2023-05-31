13ea01ce044ab07118a0d3858332951da1aac70b
Brian Gomes <bgomes@skillstorm.com>
2023-05-31 11:08:26 -0500

    last commit

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "public1" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "bjgomes-publicsubnet"
@@ -25,9 +25,9 @@ resource "aws_subnet" "public1" {
 }
 
 resource "aws_subnet" "public2" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
     Name = "bjgomes-publicsubnet"
@@ -37,19 +37,24 @@ resource "aws_subnet" "public2" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-    tags = {
+  tags = {
     Name = "bjgomes-publicroutetable"
     env  = "Dev"
   }
 }
 
 resource "aws_route" "public" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public1" {
   route_table_id = aws_route_table.public.id
-  subnet_id = [aws_subnet.public1.id, aws_subnet.public2.id]
+  subnet_id      = aws_subnet.public1.id
+}
+
+resource "aws_route_table_association" "public2" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public2.id
 }
